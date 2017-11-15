209bc1eb79af793f52b955c46e1d52bac5f4c880
Sfrancis2017 <connectsfrancis@gmail.com>
2017-11-15 16:24:44 -0500

    Added - Search & Roadmap

---
+++ results.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>CT Seniors</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php
+        include './navbar.php';
+        ?>
+        <div class="container-fluid" style="padding: 0;">
+            <iframe width="402" height="346" frameborder="0" scrolling="no" src="https://fairfield0-my.sharepoint.com/personal/sajiv_francis_fairfield_edu/_layouts/15/WopiFrame.aspx?sourcedoc={ca660ba7-a440-4486-b42b-d38c4cee2e8c}&action=embedview&ActiveCell='CT%20SENIORS%20'!A1&wdHideGridlines=True&wdHideHeaders=True&wdInConfigurator=True"></iframe>
+        </div>
+        <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </body>
+</html>
